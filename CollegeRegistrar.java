15bf16eb81bbd8987c9a80f2a4c98cda2b41176d
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2022-02-17 22:09:10 -0330

    Update CollegeRegistrar.java

--- CollegeRegistrar.java
+++ CollegeRegistrar.java
@@ -91,9 +91,58 @@ public class CollegeRegistrar
         // drops all classes for student
     }
     
+    
+    
+    @author (Joshua Kattapuram)
+    @version (16th of February, 2022)
+    
     public void populateRegistrations(String courseNumber, int section, int slot,
     String student,String professor)
     {
-                          
+        addRegistration(new Registration("COMP 1501", 0, 12, "Richard J.", "Karteek P."));
+        addRegistration(new Registration("COMP 2704", 1, 12, "Richard J.", "Olivia S."));
+        addRegistration(new Registration("PHYS 2377", 2, 12, "Richard J.", "Robert G."));
+        addRegistration(new Registration("CHEM 4927", 0, 12, "Richard J.", "Gloriatine J."));
+        addRegistration(new Registration("COMP 3444", 1, 12, "Richard J.", "Samuel F."));
+        
+        addRegistration(new Registration("COMP 1501", 0, 12, "Gordon R.", "Karteek P."));
+        addRegistration(new Registration("COMP 2704", 0, 12, "Gordon R.", "Randolph Q."));
+        addRegistration(new Registration("PHYS 2377", 1, 12, "Gordon R.", "Robert D."));
+        addRegistration(new Registration("CHEM 4927", 2, 12, "Gordon R.", "Mary J."));
+        addRegistration(new Registration("COMP 3444", 2, 12, "Gordon R.", "Peter P."));
+        
+        addRegistration(new Registration("COMP 1501", 1, 12, "Sasuke U.", "Vinicius E."));
+        addRegistration(new Registration("COMP 2704", 0, 12, "Sasuke U.", "Randolph Q."));
+        addRegistration(new Registration("PHYS 2377", 1, 12, "Sasuke U.", "Robert D."));
+        addRegistration(new Registration("CHEM 4927", 0, 12, "Sasuke U.", "Gloriatine J."));
+        addRegistration(new Registration("COMP 3444", 0, 12, "Sasuke U.", "Chad B."));
+        
+        addRegistration(new Registration("COMP 1501", 1, 12, "Sasuke U.", "Vinicius E."));
+        addRegistration(new Registration("COMP 2704", 0, 12, "Sasuke U.", "Randolph Q."));
+        addRegistration(new Registration("PHYS 2377", 1, 12, "Sasuke U.", "Robert D."));
+        addRegistration(new Registration("CHEM 4927", 2, 12, "Gordon R.", "Mary J."));
+        addRegistration(new Registration("COMP 3444", 2, 12, "Gordon R.", "Peter P.")); 
+        
+        addRegistration(new Registration("ENGL 2201", 2, 12, "Vidhi T.", "Adam S."));
+        addRegistration(new Registration("CHEM 3060", 1, 12, "Vidhi T.", "Tom C."));
+        addRegistration(new Registration("PHYS 2344", 2, 12, "Vidhi T.", "Tony S."));
+        addRegistration(new Registration("ENGL 2005", 0, 12, "Vidhi T.", "Samantha W."));
+        addRegistration(new Registration("CHEM 4927", 2, 12, "Vidhi T.", "Mary J."));
+        
+        addRegistration(new Registration("COMP 3444", 1, 12, "Sarah E.", "Samuel F."));
+        addRegistration(new Registration("COMP 2704", 1, 12, "Sarah E.", "Olivia S."));
+        addRegistration(new Registration("CHEM 3060", 1, 12, "Sarah E.", "Tom C."));
+        addRegistration(new Registration("PHYS 2344", 2, 12, "Sarah E.", "Tony S."));
+        addRegistration(new Registration("ENGL 2005", 0, 12, "Sarah E.", "Samantha W."));
+        
+    }
+    
+    public void authorInfo()
+    {
+        System.out.println("Method Author Info:");
+        System.out.println("");
+        System.out.println("populateRegistrations: Joshua Kattapuram.");
+        //you guys can add your stuff here too.
+        
     }
 }
